fe106f519ffe637cebffb1bd96469aa7951889f7
Riti <your_email@example.com>
2026-07-11 10:56:37 +0600

    modified dbhelper

--- DBHelper.cs
+++ DBHelper.cs
@@ -1,78 +1,26 @@
-﻿using System;
+﻿using System.Configuration;
 using System.Data;
-using System.Configuration;
 using Oracle.ManagedDataAccess.Client;
 
-public static class DBHelper
+namespace DigitalCertSystem
 {
-    private static string ConnStr =>
-        ConfigurationManager.ConnectionStrings["OracleDbConn"].ConnectionString;
-
-    public static DataTable ExecuteCursorProc(string procName, params OracleParameter[] parameters)
+    public static class DBHelper
     {
-        DataTable dt = new DataTable();
-        using (OracleConnection conn = new OracleConnection(ConnStr))
-        using (OracleCommand cmd = new OracleCommand(procName, conn))
+        private static string ConnStr =>
+            ConfigurationManager.ConnectionStrings["OracleDbConn"].ConnectionString;
+
+        public static OracleConnection GetConnection()
         {
-            cmd.CommandType = CommandType.StoredProcedure;
-            foreach (var p in parameters) cmd.Parameters.Add(p);
-
+            var conn = new OracleConnection(ConnStr);
             conn.Open();
-            using (OracleDataAdapter da = new OracleDataAdapter(cmd))
-            {
-                da.Fill(dt);
-            }
+            return conn;
         }
-        return dt;
-    }
 
-    public static void ExecuteNonQueryProc(string procName, params OracleParameter[] parameters)
-    {
-        using (OracleConnection conn = new OracleConnection(ConnStr))
-        using (OracleCommand cmd = new OracleCommand(procName, conn))
+        public static OracleCommand CreateCommand(string procName, OracleConnection conn)
         {
+            var cmd = new OracleCommand(procName, conn);
             cmd.CommandType = CommandType.StoredProcedure;
-            foreach (var p in parameters) cmd.Parameters.Add(p);
-
-            conn.Open();
-            cmd.ExecuteNonQuery();
+            return cmd;
         }
-    }
-
-    public static DataTable ExecuteCursorProcWithOutParam(
-        string procName, string outParamName, OracleDbType outType, int outSize,
-        out object outValue, params OracleParameter[] parameters)
-    {
-        DataTable dt = new DataTable();
-        using (OracleConnection conn = new OracleConnection(ConnStr))
-        using (OracleCommand cmd = new OracleCommand(procName, conn))
-        {
-            cmd.CommandType = CommandType.StoredProcedure;
-            foreach (var p in parameters) cmd.Parameters.Add(p);
-
-            OracleParameter outParam = new OracleParameter(outParamName, outType, outSize);
-            outParam.Direction = ParameterDirection.Output;
-            cmd.Parameters.Add(outParam);
-
-            conn.Open();
-            using (OracleDataAdapter da = new OracleDataAdapter(cmd))
-            {
-                da.Fill(dt);
-            }
-            outValue = outParam.Value;
-        }
-        return dt;
-    }
-
-    public static OracleParameter CursorOutParam(string name = "p_cursor")
-    {
-        return new OracleParameter(name, OracleDbType.RefCursor, ParameterDirection.Output);
-    }
-
-    public static OracleParameter In(string name, OracleDbType type, object value)
-    {
-        var p = new OracleParameter(name, type);
-        p.Value = value ?? DBNull.Value;
-        return p;
     }
 }
